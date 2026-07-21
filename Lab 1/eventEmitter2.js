7a39f448266748e08a56f49717d9a5374440c834
anujpratap <itsmeanuj135@gmail.com>
2026-07-21 10:31:56 +0530

    initial commit

---
+++ Lab 1/eventEmitter2.js
@@ -0,0 +1,25 @@
+import {EventEmitter} from "node:events";
+const task = new EventEmitter();
+const sayHi = (name) =>{
+    console.log(`Loggedin ${name}`);
+}; 
+task.on("greet",()=>{
+    console.log("Logged Out");
+});
+task.once("greet",()=>{
+    console.log("system started...");
+});
+
+task.on("greet",sayHi)
+
+task.on("exit",(name)=>{
+    console.log(`system is shutting down by ${name}`)
+
+})
+
+
+
+task.emit('greet','Deepanshu Tripathi')
+task.emit("greet","Sonia singh");
+task.emit("greet","Rahul Singh")
+task.off("exit","Manager");
---
+++ Lab 1/index.js
@@ -0,0 +1,14 @@
+/* Write a function to take your name and print it with hello msg */
+function sayHello(name){
+    console.log(`hello ${name}`);
+}
+
+const sayHi=(name)=>{
+    console.log("Hi",name);
+}
+sayHello("Anuj Pratap Singh");
+sayHi("Aru..");
+
+/*Take two argumnets and return their sum*/
+const twoSum =(a,b)=>a+b;
+console.log(twoSum(5,3));
