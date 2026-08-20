5acffa2f48a933132d663f58850bad8e1993d72c
anujpratap <itsmeanuj135@gmail.com>
2026-08-20 13:24:30 +0530

    lab 3

--- Lab3/app.js
+++ Lab3/app.js
@@ -1,11 +1,7 @@
 import http from "http";
 
-const server = http.createServer();
-
-server.on("request", (req, res)=> {
-    res.write("<h1>Welcome to server Side Programming</h1>");
-    res.write("<h2>Nodemon is tracking the files</h2>");
-    res.end();
+const server = http.createServer((req, res) => {
+    res.end("<h2 style='color: red;'>Welcome to the Server</h2>");
 });
 
 server.listen(5000, ()=> {
