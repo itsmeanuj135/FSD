614a43187f742fb6773b90542a925a8a2aaa29bb
anujpratap <itsmeanuj135@gmail.com>
2026-08-25 09:18:49 +0530

    commit

--- Lab3/app.js
+++ Lab3/app.js
@@ -4,6 +4,7 @@ const server = http.createServer((req, res) => {
     res.end("<h2 style='color: red;'>Welcome to the Server</h2>");
 });
 
+
 server.listen(5000, ()=> {
     console.log("Server is running");
 });
